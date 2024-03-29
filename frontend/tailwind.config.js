ce851f36b1b91ea3ccfd0c3a2509153f1640d129
Chandan <9891cky@gmail.com>
2024-03-29 17:46:57 +0530

    fixed bug in tailwind-config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,7 +16,7 @@ export default {
       },
 
       boxShadow: {
-        panelShadow: "rgba(17, 12, 46,0.15) 0px 48px 100px 0px;",
+        panelShadow: "rgba(17, 12, 46,0.15) 0px 48px 100px 0px",
       },
     },
   },
